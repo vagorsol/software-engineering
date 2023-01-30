c14a4d982905d22f7ae314c07a6c750dec623b18
Audrey Yang <audrey.butter0930@gmail.com>
2023-01-29 20:34:32 -0500

    Average itself working

--- Assignment1/AverageValue.java
+++ Assignment1/AverageValue.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -26,11 +27,41 @@ public class AverageValue {
         }
         
         String filename = args[1];
+        double sum = 0; 
+        int lineNo = 0;
         try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line = null;
             
-        } catch (IOException e) {
+            while (((line = reader.readLine()) != null)) {
+                String[] fmtLine = line.split(",");
+
+                if (fmtLine.length < N) {
+                    // checks if the Nth place on the line even exists
+                    System.out.format("There is no field %d!", N); 
+                    return; 
+                } else if(fmtLine[N].isEmpty() || fmtLine[N] == null) {
+                    // check if there is a (valid, i.e., not null or empty) value
+                    System.out.println("All elements in %s should be filled (with numbers)!");
+                    return;
+                } else {
+                    // tries to add the entry, and handles if the value is not a number 
+                    try {
+                        sum += Double.parseDouble(fmtLine[N]);
+                        lineNo++;
+                    } catch(NumberFormatException e) {
+                        System.out.format("All elements in %s should be (real) numbers!\n", filename);
+                        return;
+                    }
+                }
+            }
+        } catch (FileNotFoundException fnf){
             System.out.format("Unable to open %s! Please check that the filename is correct!\n", filename);
         }
+        catch (IOException e) {
+            System.out.format("There was a problem with reading the file! Please check that the file is formatted properly!\n", filename);
+        }
         
+        // output results - not sure if need to hold onto each field value?
+        System.out.format("The average of the values in field #%d is %.3f\n", N, sum / lineNo);
     }
 }
