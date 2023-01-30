1e31ca922992d4ddd81165cd697a9c4307171b9a
Audrey Yang <audrey.butter0930@gmail.com>
2023-01-29 20:59:33 -0500

    Defensive programming testing

--- Assignment1/AverageValue.java
+++ Assignment1/AverageValue.java
@@ -4,11 +4,9 @@ import java.io.FileReader;
 import java.io.IOException;
 
 /*
- * takes two runtime arguments: int N, and filename 
- * file = CSV file
- * calculate & display average of numerical values in Nth field of each line of file (aka column)
+ * Calculates & display the average of numerical values in the Nth field of each line of the file (aka column)
+ * NOTE: not sure if this is CSV compliant. I think it is
  */
-
 public class AverageValue {
     public static void main(String[] args) {
         // checks if user entered both filename AND field number (will not say which they forgot)
@@ -17,10 +15,14 @@ public class AverageValue {
             return; 
         }
 
-        // makes sure that args[0] is an integer
+        // makes sure that args[0] is a (positive) number
         int N;
         try {
             N = Integer.parseInt(args[0]);
+            if (N < 0) {
+                System.out.println("Please enter a POSITIVE field value!");
+                return;
+            }
         } catch(NumberFormatException e) {
             System.out.println("Field number should be an integer!");
             return;
@@ -35,9 +37,9 @@ public class AverageValue {
             while (((line = reader.readLine()) != null)) {
                 String[] fmtLine = line.split(",");
 
-                if (fmtLine.length < N) {
+                if (fmtLine.length < N + 1) {
                     // checks if the Nth place on the line even exists
-                    System.out.format("There is no field %d!", N); 
+                    System.out.format("There is no field %d!\n", N); 
                     return; 
                 } else if(fmtLine[N].isEmpty() || fmtLine[N] == null) {
                     // check if there is a (valid, i.e., not null or empty) value
@@ -49,19 +51,24 @@ public class AverageValue {
                         sum += Double.parseDouble(fmtLine[N]);
                         lineNo++;
                     } catch(NumberFormatException e) {
-                        System.out.format("All elements in %s should be (real) numbers!\n", filename);
+                        System.out.format("All elements in %s should be a number!\n", filename);
                         return;
                     }
                 }
             }
-        } catch (FileNotFoundException fnf){
+        } catch (FileNotFoundException fnf) {
             System.out.format("Unable to open %s! Please check that the filename is correct!\n", filename);
         }
         catch (IOException e) {
             System.out.format("There was a problem with reading the file! Please check that the file is formatted properly!\n", filename);
         }
         
-        // output results - not sure if need to hold onto each field value?
-        System.out.format("The average of the values in field #%d is %.3f\n", N, sum / lineNo);
+        if (lineNo == 0) {
+            System.out.println("Empty file! Please inlcude comma-separated numbers in the file to get an average!");
+        } else {
+            // output results - I am assuming we do not need to output the given field value for each line
+            System.out.format("The average of the values in field #%d is %.3f\n", N, sum / lineNo);
+        }
+        
     }
 }
