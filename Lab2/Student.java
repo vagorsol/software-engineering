ad5236c037909480e60eec0489afbccf7e828b39
Audrey Yang <audrey.butter0930@gmail.com>
2023-01-30 14:57:31 -0500

    Changes against reference copying issues in student.java

--- Lab2/Student.java
+++ Lab2/Student.java
@@ -12,7 +12,11 @@ public class Student {
 	}
 	
 	public void add(Grade g) {
-		grades.add(g); 		
+		// makes a (NEW) copy of the given date/grade to ensure not copying references
+		// (or something like that)
+		Date addD = new Date(g.date.month, g.date.day, g.date.year);
+		Grade addG = new Grade(addD, g.score);
+		grades.add(addG); 
 	}
 	
 
