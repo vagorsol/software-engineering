55411aefea13d726379c577abfc9645dacb6113b
Audrey Yang <audrey.butter0930@gmail.com>
2023-02-11 14:40:46 -0500

    Fixed comparison bug

--- Assignment1/Person.java
+++ Assignment1/Person.java
@@ -39,18 +39,27 @@ public class Person {
 
 		for (Person person : persons) {
 			if (person != null) {
+
 				// if the list is empty, just add the person 
 				if (ret.isEmpty()){
 					ret.add(person);
-				} else{
+				} else {
+					boolean toAdd = true; 
+
 					for (int i = 0; i < ret.size(); i++) {
 						Person guy = ret.get(i);
+
 						// defend against a null name/birthplace/birthdate
 						if (person.getName() != null && person.getBirthplace() != null && person.getBirthdate() != null){
-							if (!person.getName().equals(guy.getName()) && !person.getBirthplace().equals(guy.getBirthplace()) && !person.getBirthdate().equals(guy.getBirthdate())) {
-								ret.add(person);
-							}	
+							if (person.getName().equals(guy.getName()) && person.getBirthplace().equals(guy.getBirthplace()) && person.getBirthdate().equals(guy.getBirthdate())) {
+								toAdd = false;
+								break;
+							}
 						}
+					}
+
+					if(toAdd) {
+						ret.add(person);
 					}
 				}
 			}
@@ -63,21 +72,24 @@ public class Person {
 		List<Person> list = new LinkedList<>();
 		Person guyA = new Person("Todd Howard", "Florida", new Date(2013, 9, 2));
 		Person guyB = new Person("John Madden", "Ohio", new Date(2017, 8, 5));
-		Person guyC = new Person("silly", null, new Date(2017, 8, 5));
-		Person guyD = new Person("Todd Howard", "Florida", new Date(2013, 9, 2));
+		Person guyC = new Person("silly", "New York", new Date(2017, 8, 5));
 
 		list.add(guyA);
 		list.add(guyB);
 		list.add(guyC);
-		list.add(guyD);
+		list.add(guyA);
+		list.add(guyB);
+		list.add(guyA);
+		list.add(guyB);
 
 		List<Person> ret = findDistinct(list);
+
 		for (int i = 0; i < ret.size(); i++) {
 			Person person = ret.get(i);
-			System.out.println(person.getName());
+			System.out.println(i + " " + person.getName());
 		}
 
 		// check catching Null
-		ret = findDistinct(null);
+		// ret = findDistinct(null);
 	}
 }
