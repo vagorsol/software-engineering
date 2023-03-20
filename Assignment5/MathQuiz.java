66f853bc3b84fed8be122075106cf4c8462cd7dc
Audrey Yang <audrey.butter0930@gmail.com>
2023-03-19 20:35:44 -0400

    testing

--- Assignment5/MathQuiz.java
+++ Assignment5/MathQuiz.java
@@ -1,10 +1,29 @@
 public class MathQuiz {
-
-    private boolean carryOver(int x, int y){
-        return (x + y >= 10 && x + y < 20);
+    private static int test = 0;
+    private static boolean carryOver(int x, int y){
+        return x + y >= 10;
     }
     public static void main(String[] args){
-        int x = (int) Math.random();
-        System.out.println(x);
+        int a = (int) (Math.random() * 10);
+        int b = (int) (Math.random() * 10);
+        System.out.println(a + " " + b + " " + carryOver(a, b));
+        // System.out.println((int) (Math.random() * 10));
+        
+        String spaces = "    2";
+        String[] parts = spaces.split(" ");
+        for (String i:parts){
+            System.out.println(i);
+        }
+        test += 1;
+        System.out.println(test);
+
+        int[][] arr = new int[10][2];
+        for (int i = 0; i < 10; i++){
+            for (int j = 0; j<2; j++){
+                arr[i][j] = i + j;
+                System.out.print(" " + arr[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
