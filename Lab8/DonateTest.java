cd23bb33416029f4bde5274036b2779d53ba8b35
Audrey Yang <audrey.butter0930@gmail.com>
2023-03-27 15:51:39 -0400

    Updated test cases

--- Lab8/DonateTest.java
+++ Lab8/DonateTest.java
@@ -50,13 +50,26 @@ public class DonateTest {
 	@Test
 	public void testDonationOverflow() {
 		f = new Fund(100);
+		double bal1 = f.donate("user2", 20);
+		
+		assertEquals(20, bal1, 0.1); 
+		assertEquals(20, f.balance, 0.1); // balance should be equal to amt donated
+		assertEquals(1, f.donors.size());
+		assertTrue(f.donors.contains("user2"));
+
 		double bal = f.donate("user1", 1100);
 
 		// expected actual delta, because double are funny sometimes
-		assertEquals(100, bal, 0.1); // returned should be the difference b/t target (100) and balance (was 0)? 
-		assertEquals(100, f.balance, 0.1); // balance should be equal to amt donated
-		assertEquals(1, f.donors.size());
+		assertEquals(80, bal, 0.1); // returned should be the difference b/t target (100) and balance (20) (before donation)? 
+
+		// balance should be equal the target since amt donated is over
+		// ? HOWEVER, there is a bug in the code where the target amount is added to the balance (and not the donation amount)
+		// ? so it actually is 120 (yay, bugs!)
+		assertEquals(100, f.balance, 0.1); 
+
+		assertEquals(2, f.donors.size());
 		assertTrue(f.donors.contains("user1"));	
+		assertTrue(f.donors.contains("user2"));
 	}
 
 	@Test
