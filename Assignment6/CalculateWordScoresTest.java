966fb6670f702dc081841c2b5d1b8d494cb38e9b
Audrey Yang <audrey.butter0930@gmail.com>
2023-03-31 19:49:46 -0400

    Null/Empty Set tests

--- Assignment6/CalculateWordScoresTest.java
+++ Assignment6/CalculateWordScoresTest.java
@@ -1,9 +1,42 @@
 import static org.junit.Assert.*;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalculateWordScoresTest {
-    Set<Sentence> sents;
+    private Set<Sentence> sents;
+    
+    @Before
+    public void setUp() {
+        sents = new HashSet<>();
+    }
+
+    @Test
+    public void testNullSents() {
+        Map<String, Double> ret = Analyzer.calculateWordScores(null);
+
+        assertEquals(Collections.emptyMap(), ret);
+    }
+
+    @Test
+    public void testEmptySents() {
+        Map<String, Double> ret = Analyzer.calculateWordScores(Collections.emptySet()); 
+
+        assertEquals(Collections.emptyMap(), ret);
+    }
+
+    @Test
+    public void testCaseConversion() {
+        sents.add(new Sentence(2, "it It"));
+
+        Map<String, Double> ret = Analyzer.calculateWordScores(sents);
+
+        assertEquals(1, ret.size(), 0.1);
+        assertEquals(2, ret.get("it"), 0.1);
+    }
 }
