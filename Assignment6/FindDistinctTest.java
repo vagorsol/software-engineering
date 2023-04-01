8dfd6b0a6ecd3074eb5bf111ce4b4b3da957c094
Audrey Yang <audrey.butter0930@gmail.com>
2023-04-01 16:33:59 -0400

    Updated to using abstract list type

--- Assignment6/FindDistinctTest.java
+++ Assignment6/FindDistinctTest.java
@@ -1,7 +1,6 @@
 import static org.junit.Assert.*;
 
 import java.sql.Date;
-import java.util.LinkedList;
 import java.util.List;
 
 import org.junit.Test;
@@ -17,79 +16,59 @@ public class FindDistinctTest {
 
     @Test
     public void testEmptyList() {
-        List<Person> list = new LinkedList<>();
-        List<Person> ret = Person.findDistinct(list);
+        List<Person> ret = Person.findDistinct(List.of());
 
         // should return an empty list if given an empty list
         assertEquals(0, ret.size(), 0.1);
-        assertEquals(new LinkedList<>(), ret);
+        assertEquals(List.of(), ret);
     }
 
     @Test
     public void testNullEntry() {
-        List<Person> list = new LinkedList<>();
-        list.add(null);
-        List<Person> ret = Person.findDistinct(list);
+        // TODO: how null entry?
+        Person p = null;
+        List<Person> ret = Person.findDistinct(List.of(p));
 
         // should return an empty list because should ignore null
         assertEquals(0, ret.size(), 0.1);
-        assertEquals(new LinkedList<>(), ret);
+        assertEquals(List.of(), ret);
     }
 
     @Test
     public void testNullName() {
-        List<Person> list = new LinkedList<>();
         Person p = new Person(null, "birthplace", new Date(2023, 3,31));
-
-        list.add(p);
-
-        List<Person> ret = Person.findDistinct(list);
+        List<Person> ret = Person.findDistinct(List.of(p));
 
         assertEquals(0, ret.size(), 0.1);
-        assertEquals(new LinkedList<>(), ret);
+        assertEquals(List.of(), ret);
     }
 
     @Test
     public void testNullBirthplace() {
-        List<Person> list = new LinkedList<>();
         Person p = new Person("name", null, new Date(2023, 3,31));
-
-        list.add(p);
-
-        List<Person> ret = Person.findDistinct(list);
+        List<Person> ret = Person.findDistinct(List.of(p));
 
         assertEquals(0, ret.size(), 0.1);
-        assertEquals(new LinkedList<>(), ret);
+        assertEquals(List.of(), ret);
     }
 
     @Test
     public void testNullDate() {
-        List<Person> list = new LinkedList<>();
         Person p = new Person("nam", "birthplace", null);
 
-        list.add(p);
-
-        List<Person> ret = Person.findDistinct(list);
+        List<Person> ret = Person.findDistinct(List.of(p));
 
         assertEquals(0, ret.size(), 0.1);
-        assertEquals(new LinkedList<>(), ret);
+        assertEquals(List.of(), ret);
     }
     
     @Test
     public void testDuplicateEntry() {
-        List<Person> list = new LinkedList<>(); // list to execute function over
-        List<Person> exp = new LinkedList<>(); // "expected list"
-
         Person p = new Person("Todd Howard", "Florida", new Date(2013, 9, 2));
-
-        list.add(p);
-        list.add(p);
-        exp.add(p);
-
-        List<Person> ret = Person.findDistinct(list);
+        List<Person> ret = Person.findDistinct(List.of(p, p));
         
         // should return a list with only one entry because there is duplicates
         assertEquals(1, ret.size(), 0.1);
-        assertEquals(exp, ret);
+        assertEquals(List.of(p), ret);
     }
 }
