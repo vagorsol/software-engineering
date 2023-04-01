353cd6c060c14933e99e50aa22f3eb9393e55bcf
Audrey Yang <audrey.butter0930@gmail.com>
2023-04-01 16:42:54 -0400

    Updated to using abstract set type

--- Assignment6/CalculateWordScoresTest.java
+++ Assignment6/CalculateWordScoresTest.java
@@ -1,20 +1,12 @@
 import static org.junit.Assert.*;
 
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-import org.junit.Before;
 import org.junit.Test;
 
 public class CalculateWordScoresTest {
-    private Set<Sentence> sents;
-    
-    @Before
-    public void setUp() {
-        sents = new HashSet<>();
-    }
 
     @Test
     public void testNullSents() {
@@ -32,9 +24,7 @@ public class CalculateWordScoresTest {
 
     @Test
     public void testCaseConversion() {
-        sents.add(new Sentence(2, "it It"));
-
-        Map<String, Double> ret = Analyzer.calculateWordScores(sents);
+        Map<String, Double> ret = Analyzer.calculateWordScores(Set.of(new Sentence(2, "it It")));
 
         assertEquals(1, ret.size(), 0.1);
         assertEquals(2, ret.get("it"), 0.1);
