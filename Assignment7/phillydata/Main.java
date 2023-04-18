4e93a6269a8bfdbee7cf76254e56cec7674d1f67
Audrey Yang <audrey.butter0930@gmail.com>
2023-04-17 21:01:11 -0400

    setting up menu

--- Assignment7/phillydata/Main.java
+++ Assignment7/phillydata/Main.java
@@ -1,15 +1,40 @@
 package phillydata;
 
+import phillydata.common.*;
 import phillydata.data.*;
+import phillydata.ui.PhillyDataUI;
+
+import java.io.IOError;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
+
+    // function to make main look *SLIGHTLY* nicer. literally just prints out a menu
+    private static void menu() {
+        System.out.println("Menu");
+        System.out.println("1: Total Population for All ZIP Codes");
+        System.out.println("2: Fines Per Capita");
+        System.out.println("3: Average Residential Market Value");
+        System.out.println("4: Average Residential Total Liveable Area");
+        System.out.println("5: Residential Market Value Per Capita");
+        System.out.println("0: Exit");
+        System.out.print("Select an Option: ");
+    }
     public static void main(String[] args) {
         if (args.length < 4){
             System.out.println("Missing inputs!");
-            return;
+            System.out.println("Usage: java Main" +
+                "[parking violation file format] [parking violations file name]"
+                + "[property values input file name] [population input file name]");
+            return; 
         }
         if (args.length > 4) {
             System.out.println("Too many inputs!");
+            System.out.println("Usage: java Main" +
+                "[parking violation file format] [parking violations file name]"
+                + "[property values input file name] [population input file name]");
+            return;
         }
 
         ParkingReader parkingReader; 
@@ -32,8 +57,28 @@ public class Main {
         String populationFile = args[3];
 
         CSVPropertyReader propertyReader = new CSVPropertyReader(propertyFile);
-        PopulationReader popReader = new PopulationReader(populationFile);
+        PopulationReader populationReader = new PopulationReader(populationFile);
 
-    
+        // open and read files
+        List<ParkingViolation> parkingViolations;
+        List<PropertyValue> propertyValue; 
+        List<Population> populationValue;
+
+        try {
+            parkingViolations = parkingReader.readParkingData();
+            propertyValue = propertyReader.readPropertyData();
+            populationValue = populationReader.readPopulationData();
+        } catch (IllegalStateException e) {
+            System.out.println("Error reading the file!");
+            return; 
+        } catch (IOError | SecurityException e) {
+            System.out.println("Cannot open file for reading!");
+            return;
+        }
+
+        // probably need proccessors but that's me in 30 minute's issue
+        PhillyDataUI ui = new PhillyDataUI();
+
+        ui.start();
     }
 }
---
+++ Assignment7/phillydata/ui/PhillyDataUI.java
@@ -0,0 +1,60 @@
+package phillydata.ui;
+
+import java.util.Scanner;
+
+public class PhillyDataUI {
+    protected Scanner in; 
+    
+    public PhillyDataUI() {
+        in = new Scanner(System.in);
+    }
+    // function to make main look *SLIGHTLY* nicer. literally just prints out a menu
+    private static void menu() {
+        System.out.println("Menu");
+        System.out.println("1: Total Population for All ZIP Codes");
+        System.out.println("2: Fines Per Capita");
+        System.out.println("3: Average Residential Market Value");
+        System.out.println("4: Average Residential Total Liveable Area");
+        System.out.println("5: Residential Market Value Per Capita");
+        System.out.println("0: Exit");
+        System.out.print("Select an Option: ");
+    }
+
+    public void start() {
+        boolean looping = true;
+        int userInput;
+
+        while (looping) {
+            menu();
+            userInput = Integer.parseInt(in.nextLine());
+
+            // todo:set them to prrocessors (or something)
+            switch (userInput) {
+                case 0:
+                    System.out.println("Program Closing!");
+                    looping = false; 
+                    break; 
+                case 1:
+                    System.out.println("Total Population for All ZIP Codes");
+                    break;
+                case 2:
+                    System.out.println("Fines Per Capita");
+                    break;
+                case 3:
+                    System.out.println("Average Residential Market Value");
+                    break;
+                case 4: 
+                    System.out.println("Average Residential Total Liveable Area");
+                    break;
+                case 5:
+                    System.out.println("Residential Market Value Per Capita");
+                    break;
+                default:
+                    System.out.println("Invalid Option! Please select one of the listed options!");
+                    break;
+            }
+        }
+    }
+}
+
+
